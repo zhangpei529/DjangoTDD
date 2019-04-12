e7abe48eab4772eb176ba1a0d84d318f05305c87
zhangpei529 <zhangpei529@vip.qq.com>
2019-04-12 15:20:41 +0800

    创建lists的app

--- function_test.py
+++ function_test.py
@@ -1,5 +1,19 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
